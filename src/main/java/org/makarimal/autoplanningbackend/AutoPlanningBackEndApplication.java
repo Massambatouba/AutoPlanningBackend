7ce40584de3d276cde1350117dc5b1a19f18ea15
massamba08 <samndodia@gmail.com>
2025-04-29 22:47:09 +0200

    first commit

---
+++ src/main/java/org/makarimal/autoplanningbackend/AutoPlanningBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.makarimal.autoplanningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPlanningBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPlanningBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/makarimal/autoplanningbackend/AutoPlanningBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makarimal.autoplanningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPlanningBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
